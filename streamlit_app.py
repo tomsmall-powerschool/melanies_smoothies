ef81c723ab3e9fd164fca8a6174fe13a651b87df
Tom Small <tom.small@powerschool.com>
2025-05-28 15:25:30 -0400

    Update streamlit_app.py
    Show SmoothieFruit info for watermelons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -38,3 +39,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+
+# New section to display smoothiefruit nutrition information
+response = requests.get("https://my.smoothiefruit.com/api/fruit/watermelon")
+st.text(response)
