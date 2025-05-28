3ad5e47775fbf9ea26feb46990164a92472a5845
Tom Small <tom.small@powerschool.com>
2025-05-27 20:27:12 -0400

    Update streamlit_app.py
    changes to how to connect

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom smoothie!")
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 name_on_order = st.text_input('Name on smoothie:')
