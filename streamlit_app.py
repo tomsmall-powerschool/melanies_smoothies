710e271fa934aa197a723f9d7f23d773fd27625a
Tom Small <tom.small@powerschool.com>
2025-05-28 17:33:39 -0400

    Update streamlit_app.py
    Use search_on value to get nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,9 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        response = requests.get('https://www.smoothiefroot.com/api/fruit/' + search_on)
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        response = requests.get('https://www.smoothiefroot.com/api/fruit/' + fruit_chosen)
         sf_df = st.dataframe(data=response.json(), use_container_width=True)
 
     my_insert_stmt = """
