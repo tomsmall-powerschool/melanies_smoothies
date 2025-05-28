d7571cb5714bea491b22f6c678f5e94a54f64c39
Tom Small <tom.small@powerschool.com>
2025-05-28 15:32:57 -0400

    Update streamlit_app.py
    Fix typo: s/subhead/subheader/

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        st.subhead(fruit_chosen + ' Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         response = requests.get('https://www.smoothiefroot.com/api/fruit/' + fruit_chosen)
         sf_df = st.dataframe(data=response.json(), use_container_width=True)
 
