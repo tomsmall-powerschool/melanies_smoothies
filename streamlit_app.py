77ba3c675ffb48da178ab5916695ac4a8640fe01
Tom Small <tom.small@powerschool.com>
2025-05-28 17:30:57 -0400

    Update streamlit_app.py
    Get search_on for chosen fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ st.write("Choose the fruits you want in your custom smoothie!")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 
 name_on_order = st.text_input('Name on smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
@@ -24,6 +25,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         response = requests.get('https://www.smoothiefroot.com/api/fruit/' + fruit_chosen)
